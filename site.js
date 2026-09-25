43271e92974479e068f7dee4ce624418687a56a7
sven353 <sven@mulfinger.com>
2026-09-25 15:23:38 +0200

    Add files via upload

--- site.js
+++ site.js
@@ -209,8 +209,8 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     }, {
       root: null,
-      rootMargin: '0px 0px -40px 0px',
-      threshold: 0.1
+      rootMargin: '0px 0px -60px 0px',
+      threshold: 0.12
     });
 
     revealEls.forEach(function (el) { revealObserver.observe(el); });
